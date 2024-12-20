d0aac306568c3b2a1b060a594391157262a5a6cb
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-12-19 22:11:38 -0300

    commitando 19/12

---
+++ src/main/java/com/barbershop/dto/PerformedServicesDTO.java
@@ -0,0 +1,67 @@
+package com.barbershop.dto;
+
+import java.time.LocalDateTime;
+
+public class PerformedServicesDTO {
+
+	private Long id;
+
+	private Long client;
+
+	private Long services;
+
+	private Long products;
+
+	private Double price;
+
+	private LocalDateTime localDateTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getClient() {
+		return client;
+	}
+
+	public void setClient(Long client) {
+		this.client = client;
+	}
+
+	public Long getServices() {
+		return services;
+	}
+
+	public void setServices(Long services) {
+		this.services = services;
+	}
+
+	public Long getProducts() {
+		return products;
+	}
+
+	public void setProducts(Long products) {
+		this.products = products;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+
+}
