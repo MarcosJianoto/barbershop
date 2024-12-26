f4fe143f0e218cb420e9161eb9d08ef246f43ba2
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-12-25 22:22:28 -0300

    crud quase concluido do projeto da barbearia

--- src/main/java/com/barbershop/repositories/ClientRepository.java
+++ src/main/java/com/barbershop/repositories/ClientRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.barbershop.entities.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+	
 }
--- src/main/java/com/barbershop/services/PerformedServicesService.java
+++ src/main/java/com/barbershop/services/PerformedServicesService.java
@@ -1,18 +1,66 @@
 package com.barbershop.services;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.barbershop.dto.PerformedServicesDTO;
+import com.barbershop.entities.Client;
 import com.barbershop.entities.PerformedServices;
+import com.barbershop.entities.Products;
+import com.barbershop.entities.Services;
+import com.barbershop.repositories.ClientRepository;
+import com.barbershop.repositories.PerformedServicesRepository;
+import com.barbershop.repositories.ProductsRepository;
+import com.barbershop.repositories.ServicesRepository;
 
 @Service
 public class PerformedServicesService {
 
+	@Autowired
+	private PerformedServicesRepository performedServicesRepository;
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private ProductsRepository productsRepository;
+
+	@Autowired
+	private ServicesRepository servicesRepository;
+
 	public void savePerformedServices(PerformedServicesDTO performedServicesDTO) {
 
+		Optional<Client> client = clientRepository.findById(performedServicesDTO.getClient());
+		Optional<Products> products = productsRepository.findById(performedServicesDTO.getProducts());
+		Optional<Services> services = servicesRepository.findById(performedServicesDTO.getServices());
+
 		PerformedServices performedServices = new PerformedServices();
-		
-		
+
+		if (client.isPresent() && products.isPresent() || client.isPresent() && services.isPresent()) {
+
+			performedServices.setClients(client.get());
+			performedServices.setPrice(performedServicesDTO.getPrice());
+
+			if (products.isPresent()) {
+				performedServices.setProducts(products.get());
+			}
+
+			if (services.isPresent()) {
+				performedServices.setService(services.get());
+			}
+
+			performedServices.setLocalDateTime(LocalDateTime.now());
+			performedServicesRepository.save(performedServices);
+		}
+
+	}
+
+	public void deletePerformedServices(Long id) {
+		performedServicesRepository.deleteById(id);
 	}
 
 }
