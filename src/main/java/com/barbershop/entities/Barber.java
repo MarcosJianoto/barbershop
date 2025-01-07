641c9783003cb7b282e3df0921f78c8c427636b8
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-01-06 22:00:47 -0300

    Inicio da entidade Barber.java

---
+++ src/main/java/com/barbershop/entities/Barber.java
@@ -0,0 +1,51 @@
+package com.barbershop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "barber")
+public class Barber {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "barber_sequence")
+	@SequenceGenerator(name = "barber_sequence", sequenceName = "barber_sequence", allocationSize = 1)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "phone_number")
+	private String phone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
