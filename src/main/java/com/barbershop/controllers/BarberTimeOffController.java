addf06c8d1e31137cb6b5c31ec08d91fc5a07747
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-01-08 16:46:57 -0300

    alteração no BarberTimeOffController.java

---
+++ src/main/java/com/barbershop/controllers/BarberTimeOffController.java
@@ -0,0 +1,59 @@
+package com.barbershop.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.barbershop.dto.BarberTimeOffDTO;
+import com.barbershop.services.BarberTimeOffService;
+
+@RestController
+public class BarberTimeOffController {
+
+	@Autowired
+	private BarberTimeOffService barberTimeOffService;
+
+	@PostMapping("/savebarbertimeoff")
+	public ResponseEntity<Void> saveBarber(@RequestBody BarberTimeOffDTO barberTimeOffDTO) {
+
+		barberTimeOffService.saveBarberTimeOff(barberTimeOffDTO);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@PutMapping("/editbarbertimeoff")
+	public ResponseEntity<Void> editBarber(@PathVariable Long id, @RequestBody BarberTimeOffDTO barberTimeOffDTO) {
+
+		barberTimeOffService.updateBarberTimeOff(id, barberTimeOffDTO);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@GetMapping("/getbarbertimeoff/{id}")
+	public ResponseEntity<BarberTimeOffDTO> getBarberId(@PathVariable Long id) {
+
+		BarberTimeOffDTO barberTimeOffDTO = barberTimeOffService.getBarberTimeOffId(id);
+		return ResponseEntity.ok().body(barberTimeOffDTO);
+	}
+
+	@GetMapping("/getbarbertimeoff")
+	public ResponseEntity<List<BarberTimeOffDTO>> getBarberTimeOff(@PathVariable Long id) {
+
+		List<BarberTimeOffDTO> barberTimeOffDTOs = barberTimeOffService.getListBarberTimeOff();
+		return ResponseEntity.ok().body(barberTimeOffDTOs);
+	}
+
+	@DeleteMapping("/deletebarbertimeoff")
+	public void deleteBarber(@PathVariable Long id) {
+		barberTimeOffService.deleteBarberTimeOff(id);
+	}
+
+}
